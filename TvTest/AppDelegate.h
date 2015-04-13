0b5363cbdb621eb09856ffbbe4898a1497afbe21
Toby Glenn Peters <Toby.Peters@capitalone.com>
2015-04-13 11:48:03 -0400

    Initial Commit

---
+++ TvTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TvTest
+//
+//  Created by Peters, Toby on 4/13/15.
+//  Copyright (c) 2015 Peters, Toby. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TvTest/MasterViewController.h
@@ -0,0 +1,19 @@
+//
+//  MasterViewController.h
+//  TvTest
+//
+//  Created by Peters, Toby on 4/13/15.
+//  Copyright (c) 2015 Peters, Toby. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+
+@end
+
